46375c13c16ed52c9a6efac5ccd21e808fed59c7
Guillermo Saenz <g42v@me.com>
2015-01-20 22:20:06 -0500

    Initial Commit

---
+++ PassDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PassDay
+//
+//  Created by Guillermo Saenz on 1/20/15.
+//  Copyright (c) 2015 Property Atomic Strong SAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PassDay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PassDay
+//
+//  Created by Guillermo Saenz on 1/20/15.
+//  Copyright (c) 2015 Property Atomic Strong SAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
