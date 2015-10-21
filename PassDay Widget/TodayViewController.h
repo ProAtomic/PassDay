29267ee9c08ffe4e3f442d4053e02514991ce9f7
Guillermo Saenz <gsaenz@proatomicdev.com>
2015-10-20 23:09:38 -0500

    Added Today extension

---
+++ PassDay Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  PassDay Widget
+//
+//  Created by Guillermo Saenz on 10/20/15.
+//  Copyright © 2015 Property Atomic Strong SAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
